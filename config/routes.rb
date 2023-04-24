a5640b4eb2f2b636d5c0fe5dc559b80851e5178c
Gautier Tiehoule <gautier.tiehoule@ngser.com>
2023-04-24 11:49:26 +0000

    Feat: Add route to MTN USSD

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  # root "articles#index"
+  match 'mtn/ussd/vignette', to: 'mtn#index', via: [:get, :post]
 end
