a959a495206e2a676f0666c162d118856030cd7f
Gautier Tiehoule <gautier.tiehoule@ngser.com>
2023-04-24 12:15:51 +0000

    Feat: Add Orange Ussd route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   match 'mtn/ussd/vignette', to: 'mtn#index', via: [:get, :post]
+  match 'orange/ussd/vignette', to: 'orange#index', via: [:get, :post]
 end
