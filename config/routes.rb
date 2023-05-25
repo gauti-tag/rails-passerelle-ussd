7c2dc27433a79b7879a8a3e1e96e73242314d481
Gautier Tiehoule <gautier.tiehoule@ngser.com>
2023-05-25 13:12:24 +0000

    Feat: Add Route to deal with USSD Menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  match 'mtn/ussd/vignette', to: 'mtn#index', via: [:get, :post]
-  match 'orange/ussd/vignette', to: 'orange#index', via: [:get, :post]
+  match 'mtn/ussd/redevance', to: 'mtn#index', via: [:get, :post]
+  match 'orange/ussd/redevance', to: 'orange#index', via: [:get, :post]
 end
